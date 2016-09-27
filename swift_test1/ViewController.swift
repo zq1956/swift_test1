4ef21c72c042a22733c64319e67820b6f4f4616b
zq1956 <zq1956@qdemacbook-pro.local>
2016-09-27 17:15:37 +0800

    first iOS project

--- swift_test1/ViewController.swift
+++ swift_test1/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +20,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func setDefaultLabelText(sender:UIButton)
+    {
+        
+    }
 
-
+    @IBAction func sayHello(_ sender:AnyObject)
+    {
+    }
 }
 
